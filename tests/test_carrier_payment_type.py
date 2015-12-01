f251cad480bc22b51a8370550ee79a1f677cc747
resteve <resteve@zikzakmedia.com>
2015-12-01 11:04:22 +0100

    Tests

--- tests/test_carrier_payment_type.py
+++ tests/test_carrier_payment_type.py
@@ -1,37 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the carrier_payment_type module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends, test_view
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class CarrierPaymentTypeTestCase(unittest.TestCase):
-    '''
-    Test Carrier Payment Type module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('carrier_payment_type')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('carrier_payment_type')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class CarrierPaymentTypeTestCase(ModuleTestCase):
+    'Test Carrier Payment Type module'
+    module = 'carrier_payment_type'
 
 
 def suite():
@@ -39,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         CarrierPaymentTypeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
