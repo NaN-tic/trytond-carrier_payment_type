1cc2c8a5505a920b71ca042776d4592b7ca4cae9
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:21:02 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part of the carrier_payment_type module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-from .test_carrier_payment_type import suite
-
-__all__ = ['suite']
